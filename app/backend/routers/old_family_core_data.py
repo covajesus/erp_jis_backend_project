083fc20561b480fc19a260d87802a4432692fc43
Jesús Cova <jesuscova@jisparking.com>
2023-11-03 10:01:11 -0300

    General Changes

--- app/backend/routers/old_family_core_data.py
+++ app/backend/routers/old_family_core_data.py
@@ -5,6 +5,7 @@ from app.backend.schemas import UserLogin
 from app.backend.classes.old_family_core_datum_class import OldFamilyCoreDatumClass
 from app.backend.classes.family_core_datum_class import FamilyCoreDatumClass
 from app.backend.auth.auth_user import get_current_active_user
+import json
 
 old_family_core_data = APIRouter(
     prefix="/old_family_core_data",
@@ -14,18 +15,19 @@ old_family_core_data = APIRouter(
 @old_family_core_data.post("/transfer/{rut}")
 def transfer(rut: int, session_user: UserLogin = Depends(get_current_active_user), db: Session = Depends(get_db)):
     family_data = FamilyCoreDatumClass(db).get('employee_rut', rut, 2)
+    family_data = json.loads(family_data)
 
     for family_datum in family_data:
         old_family_inputs = {
-            'family_type_id': family_datum.family_type_id,
-            'employee_rut': family_datum.employee_rut,
-            'gender_id': family_datum.gender_id,
-            'rut': family_datum.rut,
-            'names': family_datum.names,
-            'father_lastname': family_datum.father_lastname,
-            'mother_lastname': family_datum.mother_lastname,
-            'born_date': family_datum.born_date,
-            'support': family_datum.support,
+            'family_type_id': family_datum['family_type_id'],
+            'employee_rut': family_datum['employee_rut'],
+            'gender_id': family_datum['gender_id'],
+            'rut': family_datum['rut'],
+            'names': family_datum['names'],
+            'father_lastname': family_datum['father_lastname'],
+            'mother_lastname': family_datum['mother_lastname'],
+            'born_date': family_datum['born_date'],
+            'support': family_datum['support'],
         }
 
         OldFamilyCoreDatumClass(db).store(old_family_inputs)
