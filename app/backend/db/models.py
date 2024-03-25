e369ac0af0982b0f7bda538b0f0f2c25bb294d32
Jesús Cova <jesuscova@jisparking.com>
2024-03-25 13:45:32 -0300

    cambios

--- app/backend/db/models.py
+++ app/backend/db/models.py
@@ -288,6 +288,7 @@ class UserModel(Base):
     rut = Column(Integer)
     visual_rut = Column(String(20))
     nickname = Column(String(255))
+    password = Column(Text())
     hashed_password = Column(Text())
     disabled = Column(Integer)
     added_date = Column(DateTime())
